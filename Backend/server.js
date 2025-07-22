a1a5af86ee4dca705e58deddb148d968c77982b9
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-22 20:37:37 +0500

    Added Error Handling

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const detenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is Running`));
