a3f20ca4e0a7d090920b244f12af005e85376a86
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-24 19:03:45 +0500

    UI Done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import App from "./App";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Auth from "./pages/Auth";
+import Dashboard from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App, // This App will act as a layout
+    children: [
+      { index: true, Component: Dashboard },
+      {
+        path: "auth",
+        Component: Auth,
+        children: [
+          { path: "login", Component: Login },
+          { path: "register", Component: Register },
+        ],
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
