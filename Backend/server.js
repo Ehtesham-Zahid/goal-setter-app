75be3681bc7c369c9d7f5f6b23372a0096ed09e2
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-25 23:19:38 +0500

    UI Done

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// const path = require("path");
 const colors = require("colors");
 const cors = require("cors");
 const detenv = require("dotenv").config();
@@ -18,6 +19,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 
 app.use(errorHandler);
 
