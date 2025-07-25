7df5ac9864556ac8d4c5188ce11494ccb465ba8a
Ehtesham-Zahid <shamimalick321@gmail.com>
2025-07-25 19:02:57 +0500

    Redirect to login

--- Backend/controllers/goalControllers.js
+++ Backend/controllers/goalControllers.js
@@ -27,16 +27,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   // Make sure the looged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
@@ -56,16 +54,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   // Make sure the looged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../contexts/AuthContext";
+
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/auth/login");
+    }
+  }, [user, navigate]);
+
   return <div>Dashboard</div>;
 };
 
